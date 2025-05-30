e8ab5269c2a676310a94708bb2ad833ec2882928
Tom Lotin <etooms0@gmail.com>
2025-05-30 17:47:08 +0200

    Ajout entité log quand shadow clone cbbv

--- src/main/java/com/sekwah/narutomod/abilities/jutsus/ShadowCloneAbility.java
+++ src/main/java/com/sekwah/narutomod/abilities/jutsus/ShadowCloneAbility.java
@@ -1,12 +1,18 @@
 package com.sekwah.narutomod.abilities.jutsus;
 
+import com.sekwah.narutomod.entity.SubstitutionLogEntity;
+
 import com.sekwah.narutomod.abilities.Ability;
 import com.sekwah.narutomod.capabilities.INinjaData;
 import com.sekwah.narutomod.entity.jutsuprojectile.FireballJutsuEntity;
 import com.sekwah.narutomod.sounds.NarutoSounds;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
@@ -38,6 +44,8 @@ public class ShadowCloneAbility extends Ability implements Ability.Cooldown {
         ninjaData.scheduleDelayedTickEvent((delayedPlayer) -> {
 
             System.out.println("[+] - Shadow Cone Jutsu launched !");
+            spawnCloneAt(player, player.position(), ninjaData);
+            System.out.println("[+] - Fini !");
         }, 10);
     }
 
@@ -45,4 +53,23 @@ public class ShadowCloneAbility extends Ability implements Ability.Cooldown {
     public int getCooldown() {
         return 3 * 20;
     }
+
+    public void spawnCloneAt(Player player, Vec3 pos, INinjaData ninjaData) {
+        ninjaData.setInvisibleTicks(5);
+        player.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 5, 0, false, false));
+        SubstitutionLogEntity log = new SubstitutionLogEntity(player.level());
+        log.setPos(pos.add(0, 1, 0));
+        player.level().addFreshEntity(log);
+        if(player.level() instanceof ServerLevel serverLevel) {
+            serverLevel.sendParticles(ParticleTypes.CLOUD,
+                    pos.x,
+                    pos.y + (player.getBbHeight() / 2),
+                    pos.z,
+                    100,
+                    0.5, 0.7, 0.5, 0);
+        }
+    }
+
+
+
 }
